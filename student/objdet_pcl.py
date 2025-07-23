09c54e9a03b64bb35ef8841efacde0d017180662
acastelan2 <antcas22@gmail.com>
2025-07-23 04:14:29 -0500

    Step 1 done

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -14,6 +14,7 @@
 import cv2
 import numpy as np
 import torch
+import open3d as o3d
 
 # add project directory to python path to enable relative imports
 import os
@@ -29,6 +30,12 @@ from tools.waymo_reader.simple_waymo_open_dataset_reader import dataset_pb2, lab
 # object detection tools and helper functions
 import misc.objdet_tools as tools
 
+def close_visualization(vis):
+    """
+    Callback function to close the visualizer when the right arrow key is pressed.
+    This function will return False, which tells the visualizer to stop rendering and close.
+    """
+    return False 
 
 # visualize lidar point-cloud
 def show_pcl(pcl):
@@ -38,15 +45,24 @@ def show_pcl(pcl):
     print("student task ID_S1_EX2")
 
     # step 1 : initialize open3d with key callback and create window
-    
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window()
+
     # step 2 : create instance of open3d point-cloud class
+    pcd = o3d.geometry.PointCloud()
 
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
+    pcd.points = o3d.utility.Vector3dVector(pcl[:, :3])
 
     # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
-    
-    # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
+    vis.add_geometry(pcd)
+    vis.update_geometry(pcd)
 
+    # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
+    vis.register_key_callback(o3d.visualization.gui.KeyName.RIGHT, close_visualization)
+
+    vis.run()
+    vis.destroy_window()
     #######
     ####### ID_S1_EX2 END #######     
        
@@ -59,18 +75,26 @@ def show_range_image(frame, lidar_name):
     print("student task ID_S1_EX1")
 
     # step 1 : extract lidar data and range image for the roof-mounted lidar
-    
-    # step 2 : extract the range and the intensity channel from the range image
+    lidar = waymo_utils.get(frame.lasers, lidar_name)
+    ri,_,_ = waymo_utils.parse_range_image_and_camera_projection(lidar)
     
     # step 3 : set values <0 to zero
-    
+    ri[ri<0]=0.0
+
+    # step 2 : extract the range and the intensity channel from the range image
+    ri_range = ri[:,:,0]
+    ri_intensity = ri[:,:,1]
+
     # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
+    ri_range = ri_range * 255 / (np.amax(ri_range) - np.amin(ri_range))
+    ri_range = ri_range.astype(np.uint8)
     
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
-    
-    # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
-    
-    img_range_intensity = [] # remove after implementing all steps
+    ri_intensity = ri_intensity * 255 / (np.percentile(ri_intensity, 99) - np.percentile(ri_intensity, 1)) 
+    ri_intensity = ri_intensity.astype(np.uint8)
+
+    # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer    
+    img_range_intensity = np.vstack((ri_range, ri_intensity)).astype(np.uint8)
     #######
     ####### ID_S1_EX1 END #######     
     
